325f8fe7ba4ba9f814ba90499ed06455752d18b1
2000sagarr <2000sagarr@gmail.com>
2023-08-07 17:48:18 +0530

    Starting commits

---
+++ src/core/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { API } from "../backend";
+// import "../styles.css";
+function Home() {
+  return (
+    <div>
+      <h1 className="text-white">Home{API}</h1>
+    </div>
+  );
+}
+
+export default Home;
