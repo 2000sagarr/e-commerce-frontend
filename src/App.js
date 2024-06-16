8916e567527d6268c580141ad7e87801fd744155
2000sagarr <2000sagarr@gmail.com>
2024-06-16 13:23:51 +0530

    Order page , tracker, and details page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Cart from "./customer/components/cart/Cart.jsx";
 import Checkout from "./customer/components/checkout/Checkout.jsx";
 import Footer from "./customer/components/footer/Footer.jsx";
 import Navigation from "./customer/components/navigation/Navigation.jsx";
+import Order from "./customer/components/order/Order.jsx";
+import OrderDetails from "./customer/components/order/OrderDetails.jsx";
 import Product from "./customer/components/product/Product.jsx";
 import ProductDetails from "./customer/components/productDetails/ProductDetails.jsx";
 import Home from "./customer/pages/Home";
@@ -15,7 +17,9 @@ function App() {
         {/* <ProductDetails /> */}
         {/* <Home /> */}
         {/* <Cart /> */}
-        <Checkout />
+        {/* <Checkout /> */}
+        {/* <Order /> */}
+        <OrderDetails />
       </div>
       <Footer />
     </div>
