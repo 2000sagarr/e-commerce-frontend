3fab20c2850ba961876be1f5c4eae964d1f0d62a
2000sagarr <2000sagarr@gmail.com>
2024-07-19 22:44:04 +0530

    Issue fix

--- src/customer/components/cart/CartItem.jsx
+++ src/customer/components/cart/CartItem.jsx
@@ -1,9 +1,10 @@
-import { Button, IconButton } from "@mui/material";
 import React from "react";
+import { Button } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { removeCartItem, updateCartItem } from "../../../Redux/Customers/Cart/Action";
+import { IconButton } from "@mui/material";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
-import { useDispatch } from "react-redux";
-import { removeCartItem, updateCartItem } from "../../../state/customers/Cart/Action";
 
 const CartItem = ({ item,showButton }) => {
   const dispatch = useDispatch();
@@ -13,17 +14,17 @@ const CartItem = ({ item,showButton }) => {
     const data = { cartItemId: item?.id, jwt };
     dispatch(removeCartItem(data));
   };
-  const handleUpdateCartItem = (num) => {
+  const handleUpdateCartItem=(num)=>{
     const data={data:{quantity:item.quantity+num}, cartItemId:item?.id, jwt}
     dispatch(updateCartItem(data))
-  };
+  }
   return (
     <div className="p-5 shadow-lg border rounded-md">
       <div className="flex items-center">
         <div className="w-[5rem] h-[5rem] lg:w-[9rem] lg:h-[9rem] ">
-        <img
+          <img
             className="w-full h-full object-cover object-top"
-            src={item?.product?.imageUrl}
+            src={item?.product.imageUrl}
             alt=""
           />
         </div>
@@ -32,40 +33,35 @@ const CartItem = ({ item,showButton }) => {
           <p className="opacity-70">Size: {item?.size},White</p>
           <p className="opacity-70 mt-2">Seller: {item?.product?.brand}</p>
           <div className="flex space-x-2 items-center pt-3">
-            <p className="opacity-50 line-through">₹{item?.product?.price}</p>
+            <p className="opacity-50 line-through">₹{item?.product.price}</p>
             <p className="font-semibold text-lg">
-              ₹{item?.product?.discountedPrice}
+              ₹{item?.product.discountedPrice}
             </p>
             <p className="text-green-600 font-semibold">
-              {item?.product?.discountPersent}% off
+              {item?.product.discountPersent}% off
             </p>
           </div>
         </div>
       </div>
-      <div className="lg:flex items-center lg:space-x-10 pt-4">
+     {showButton&& <div className="lg:flex items-center lg:space-x-10 pt-4">
         <div className="flex items-center space-x-2 ">
-          <IconButton
-            onClick={() => handleUpdateCartItem(-1)}
-            color="primary"
-            aria-label="add an alarm"
-          >
+          <IconButton onClick={()=>handleUpdateCartItem(-1)} disabled={item?.quantity<=1} color="primary" aria-label="add an alarm">
             <RemoveCircleOutlineIcon />
           </IconButton>
+
           <span className="py-1 px-7 border rounded-sm">{item?.quantity}</span>
-          <IconButton
-            onClick={() => handleUpdateCartItem(1)}
-            color="primary"
-            aria-label="add an alarm"
-          >
+          <IconButton onClick={()=>handleUpdateCartItem(1)} color="primary" aria-label="add an alarm">
             <AddCircleOutlineIcon />
           </IconButton>
         </div>
         <div className="flex text-sm lg:text-base mt-5 lg:mt-0">
+          
           <Button onClick={handleRemoveItemFromCart} variant="text">
             Remove{" "}
           </Button>
+          
         </div>
-      </div>
+      </div>}
     </div>
   );
 };
--- src/customer/components/checkout/OrderSummary.jsx
+++ src/customer/components/checkout/OrderSummary.jsx
@@ -1,53 +1,55 @@
 import React from "react";
-import { Button } from "@mui/material";
+import { Badge, Button } from "@mui/material";
 import { useLocation, useNavigate } from "react-router-dom";
+import CartItem from "../Cart/CartItem";
 import { useEffect } from "react";
-import AddressCard from "../addressCard/AddressCard";
-import CartItem from "../cart/CartItem";
 import { useDispatch, useSelector } from "react-redux";
-import { getOrderById } from "../../../state/customers/Order/Action";
-import { createPayment } from "../../../state/customers/Payment/Action";
+import { getOrderById } from "../../../Redux/Customers/Order/Action";
+import AddressCard from "../adreess/AdreessCard";
+import { createPayment } from "../../../Redux/Customers/Payment/Action";
 
 const OrderSummary = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const orderId = searchParams.get("order_id");
-  const dispatch=useDispatch();
-    const jwt=localStorage.getItem("jwt");
-  const { order } = useSelector((state) => state);
+const orderId = searchParams.get("order_id");
+const dispatch=useDispatch();
+  const jwt=localStorage.getItem("jwt");
+  const {order}=useSelector(state=>state)
 
-  console.log("orderId ", order.order);
+console.log("orderId ", order.order)
 
-  useEffect(() => {
-      dispatch(getOrderById(orderId))
-  }, [orderId]);
+useEffect(()=>{
+  
+  dispatch(getOrderById(orderId))
+},[orderId])
 
-  const handleCreatePayment = () => {
-      const data={orderId:order.order?.id,jwt}
-      dispatch(createPayment(data))
-  };
+const handleCreatePayment=()=>{
+  const data={orderId:order.order?.id,jwt}
+  dispatch(createPayment(data))
+}
+  
 
   return (
     <div className="space-y-5">
-      <div className="p-5 shadow-lg rounded-md border ">
-        <AddressCard address={order.order?.shippingAddress}/>
-      </div>
-      <div>
-      <div className="lg:grid grid-cols-3 relative">
-        <div className="lg:col-span-2 bg-white">
+        <div className="p-5 shadow-lg rounded-md border ">
+            <AddressCard address={order.order?.shippingAddress}/>
+        </div>
+      <div className="lg:grid grid-cols-3 relative justify-between">
+        <div className="lg:col-span-2 ">
           <div className=" space-y-3">
-          {order.order?.orderItems.map((item) => (
+            {order.order?.orderItems.map((item) => (
               <>
                 <CartItem item={item} showButton={false}/>
               </>
             ))}
           </div>
         </div>
-        <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0 ">
+        <div className="sticky top-0 h-[100vh] mt-5 lg:mt-0 ml-5">
           <div className="border p-5 bg-white shadow-lg rounded-md">
             <p className="font-bold opacity-60 pb-4">PRICE DETAILS</p>
             <hr />
+
             <div className="space-y-3 font-semibold">
               <div className="flex justify-between pt-3 text-black ">
                 <span>Price ({order.order?.totalItem} item)</span>
@@ -74,12 +76,11 @@ const OrderSummary = () => {
               type="submit"
               sx={{ padding: ".8rem 2rem", marginTop: "2rem", width: "100%" }}
             >
-             Payment
+              Payment
             </Button>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
